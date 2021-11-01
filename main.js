9fbe4fa791db06701d401a548126213e16cd2b2e
vino <vino0908@outlook.com>
2021-11-01 16:57:10 +0800

    最简单的路由跳转

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ const number = window.location.hash.substr(1);
 const div = document.querySelector(`#div${number}`);
 div.style.display = "block";
 app.appendChild(div);
+
+window.addEventListener("hashchange", () => {
+  const number = window.location.hash.substr(1);
+  const div = document.querySelector(`#div${number}`);
+  div.style.display = "block";
+  app.children[0].style.display = "none";
+  document.body.appendChild(app.children[0]);
+  app.appendChild(div);
+});
