cdb702fd25b708bc6758e13dd86b23575d743859
vino <vino0908@outlook.com>
2021-11-01 17:07:20 +0800

    添加默认路由和404路由

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 import "./style.css";
 
 const app = document.querySelector("#app");
-const number = window.location.hash.substr(1);
-const div = document.querySelector(`#div${number}`);
-div.style.display = "block";
-app.appendChild(div);
+const number = window.location.hash.substr(1) || 1;
+let div = document.querySelector(`#div${number}`);
+if (div) {
+  div.style.display = "block";
+  app.appendChild(div);
+} else {
+  div = document.querySelector("#div404");
+  div.style.display = "block";
+  app.appendChild(div);
+}
 
 window.addEventListener("hashchange", () => {
   const number = window.location.hash.substr(1);
-  const div = document.querySelector(`#div${number}`);
-  div.style.display = "block";
+  div = document.querySelector(`#div${number}`);
+  if (div) {
+    div.style.display = "block";
+    app.appendChild(div);
+  } else {
+    div = document.querySelector("#div404");
+    div.style.display = "block";
+    app.appendChild(div);
+  }
   app.children[0].style.display = "none";
   document.body.appendChild(app.children[0]);
   app.appendChild(div);
